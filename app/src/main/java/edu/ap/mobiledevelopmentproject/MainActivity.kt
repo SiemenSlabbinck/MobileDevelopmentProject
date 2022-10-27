c726968d4495fe97446e5b1a27a84294e47ccbc4
SiemenSlabbinck <Siemen.Slabbinck@student.ap.be>
2022-10-27 17:35:29 +0200

    Add fetchJson
    add fetch and parse json

--- app/src/main/java/edu/ap/mobiledevelopmentproject/MainActivity.kt
+++ app/src/main/java/edu/ap/mobiledevelopmentproject/MainActivity.kt
@@ -1,15 +1,11 @@
 package edu.ap.mobiledevelopmentproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.JsonReader
-import android.widget.Toast
-import com.beust.klaxon.JsonArray
-import com.beust.klaxon.JsonObject
+import androidx.appcompat.app.AppCompatActivity
 import com.beust.klaxon.Parser
-import org.json.JSONObject
-import java.io.StringReader
-import java.net.URL
+import com.google.gson.GsonBuilder
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,11 +16,76 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        databaseHelper = DatabaseHelper(this);
-        val apiResponse = URL("https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_publiek1/MapServer/8/query?where=1%3D1&outFields=*&outSR=4326&f=json").readText()
-        val arr = parser.parse(apiResponse) as JsonArray<JSONObject>
-        for (obj in arr){
-            databaseHelper!!.addToilet(obj)
-        }
+        fetchJson()
+
+        //databaseHelper = DatabaseHelper(this);
+
+        // arr = parser.parse(apiResponse) as JsonArray<JSONObject>
+        //for (obj in arr){
+        //    databaseHelper!!.addToilet(obj)
+        //}
     }
+
+    fun fetchJson(){
+        println("Attempting to fetch JSON")
+        val url = "https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_publiek1/MapServer/8/query?outFields=*&where=1%3D1&f=geojson"
+
+        val request = Request.Builder().url(url).build()
+
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed to execute request")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+                println(body)
+
+                val gson = GsonBuilder().create()
+
+                val toilet = (gson.fromJson(body, Toilet::class.java)).features
+            }
+        })
+
+    }
+}
+
+class Feature {
+    var geometry: Geometry? = null
+    var properties: Properties? = null
+}
+
+class Geometry {
+    var coordinates: ArrayList<Double>? = null
+}
+
+class Properties {
+    var OMSCHRIJVING: String? = null
+
+    var TYPE: String? = null
+
+    var BETALEND: String? = null
+
+    var STRAAT: String? = null
+
+    var HUISNUMMER: String? = null
+
+    var POSTCODE = 0
+
+    var DISTRICT: String? = null
+
+    var DOELGROEP: String? = null
+
+    var INTEGRAAL_TOEGANKELIJK: String? = null
+
+    var LUIERTAFEL: String? = null
+
+    var X_COORD = 0.0
+
+    var Y_COORD = 0.0
+}
+
+class Toilet {
+    var features: ArrayList<Feature>? = null
 }
