f09e6ac3e00b37128c9e957a3bec918d3c661236
Jarno123 <jarno.bakker@hotmail.nl>
2022-11-04 11:45:04 +0100

    all screens designed

--- app/src/main/java/edu/ap/mobiledevelopmentproject/AddLocation.kt
+++ app/src/main/java/edu/ap/mobiledevelopmentproject/AddLocation.kt
@@ -1,11 +1,41 @@
 package edu.ap.mobiledevelopmentproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioGroup
 
 class AddLocation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_location)
+        var available = findViewById<RadioGroup>(R.id.availableGroup)
+        var btnSaveLocation = findViewById<Button>(R.id.btnSaveLocation)
+        var btn_goBack = findViewById<Button>(R.id.btn_goBack)
+
+        btnSaveLocation.setOnClickListener {
+            var checkedId = available.checkedRadioButtonId;
+            if(checkedId == -1) {
+                // No radio buttons are checked
+                Message.message(applicationContext, "Selecteer een optie")
+            } else {
+                // One of the options is checked
+                findRadioButton(checkedId);
+            }
+        }
+
+        //Clear values
+        btn_goBack.setOnClickListener {
+            available.clearCheck()
+        }
+
     }
+
+    private fun findRadioButton(checkedId: Int) {
+        when (checkedId) {
+            R.id.isAvailable -> return //add to JSON to save
+        }
+    }
+
 }
---
+++ app/src/main/java/edu/ap/mobiledevelopmentproject/Message.kt
@@ -0,0 +1,13 @@
+package edu.ap.mobiledevelopmentproject
+
+import android.content.Context
+import android.widget.Toast
+
+class Message {
+    companion object {
+        fun message(context: Context, msg:String) {
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
