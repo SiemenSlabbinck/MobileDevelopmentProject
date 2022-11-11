c129b353c8f421672851c53109e105f768e5cd78
Jarno123 <jarno.bakker@hotmail.nl>
2022-11-11 11:24:46 +0100

    able to see map

--- app/src/main/java/edu/ap/mobiledevelopmentproject/MapView.kt
+++ app/src/main/java/edu/ap/mobiledevelopmentproject/MapView.kt
@@ -1,15 +1,59 @@
 package edu.ap.mobiledevelopmentproject
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.osmdroid.config.Configuration
+import org.osmdroid.events.MapEventsReceiver
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.ItemizedIconOverlay
+import org.osmdroid.views.overlay.ItemizedOverlay
+import org.osmdroid.views.overlay.MapEventsOverlay
+import org.osmdroid.views.overlay.OverlayItem
+import java.io.File
+import java.net.URL
+import java.util.ArrayList
 
 class MapView : AppCompatActivity() {
+
+    private lateinit var mMapView: MapView
+    private var mMyLocationOverlay: ItemizedOverlay<OverlayItem>? = null
+    private var items = ArrayList<OverlayItem>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val osmConfig = Configuration.getInstance()
+        osmConfig.userAgentValue = packageName
+        val basePath = File(cacheDir.absolutePath, "osmdroid")
+        osmConfig.osmdroidBasePath = basePath
+        val tileCache = File(osmConfig.osmdroidBasePath, "tile")
+        osmConfig.osmdroidTileCache = tileCache
         setContentView(R.layout.activity_map_view)
+        mMapView = findViewById(R.id.mapview)
+
+
+
+        if (hasPermissions()) {
+            initMap()
+        }
+        else {
+            ActivityCompat.requestPermissions(this, arrayOf(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION), 100)
+        }
     }
+
 
     fun goBack(view:View) {
         finish()
@@ -18,5 +62,43 @@ class MapView : AppCompatActivity() {
 
     override fun finish(): Unit {
         super.finish()
+    }
+
+    private fun hasPermissions(): Boolean {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun initMap() {
+        mMapView?.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE)
+        // add receiver to get location from tap
+        val mReceive: MapEventsReceiver = object : MapEventsReceiver {
+            override fun singleTapConfirmedHelper(p: GeoPoint): Boolean {
+                Toast.makeText(baseContext, p.latitude.toString() + " - " + p.longitude, Toast.LENGTH_LONG).show()
+                return false
+            }
+
+            override fun longPressHelper(p: GeoPoint): Boolean {
+                return false
+            }
+        }
+        mMapView?.overlays?.add(MapEventsOverlay(mReceive))
+
+
+        mMapView?.controller?.setZoom(17.0)
+        // default = Ellermanstraat 33
+        setCenter(GeoPoint(51.23020595, 4.41655480828479), "Campus Ellermanstraat")
+    }
+
+    private fun addMarker(geoPoint: GeoPoint, name: String) {
+        items.add(OverlayItem(name, name, geoPoint))
+        mMyLocationOverlay = ItemizedIconOverlay(items, null, applicationContext)
+        mMapView?.overlays?.add(mMyLocationOverlay)
+    }
+
+    private fun setCenter(geoPoint: GeoPoint, name: String) {
+        mMapView?.controller?.setCenter(geoPoint)
+        addMarker(geoPoint, name)
     }
 }
